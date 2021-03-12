578c1c55bedd01bce5e3d01023bff3f5c4c40992
Trace Brandt <trpbr92@gmail.com>
2021-03-12 16:50:46 -0600

    button handlers functioning

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,35 @@
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('JQ');
+    //click handlers
+    $('#addButton').on('click', addNum);
+    $('#subtractButton').on('click', subtractNum);
+    $('#multiplyButton').on('click', multiplyNum);
+    $('#divideButton').on('click', divideNum);
+    $('#equalsButton').on('click', equalsNum);
+    $('#clearButton').on('click', clearNum);
+}//end onReady
+
+function addNum(){
+    console.log('in addNum');
+}
+
+function subtractNum(){
+    console.log('in subtractNum');
+}
+
+function multiplyNum(){
+    console.log('in multiplyNum');
+}
+
+function divideNum(){
+    console.log('in divideNum');
+}
+
+function equalsNum(){
+    console.log('in equalsNum');
+}
+
+function clearNum(){
+    console.log('in clearNum');
 }
