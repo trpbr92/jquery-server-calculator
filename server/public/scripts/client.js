90844f3f519006ece8bc2c50c592bad2b968aaa1
Trace Brandt <trpbr92@gmail.com>
2021-03-13 11:26:52 -0600

    mild css styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,5 @@
 $(document).ready(onReady);
-operation = "";
+let operation = "";
 function onReady(){
     //click handlers
     $('#addButton').on('click', addNum);
@@ -9,60 +9,47 @@ function onReady(){
     $('#equalsButton').on('click', equalsNum);
 }//end onReady
 
-
-
 function addNum(){
     operation = "+";
     console.log('in addNum');
-  let addNumObject ={
-      num1: $('#firstNumber').val(),
-      num2: $('#secondNumber').val()
-  }//end addNumObject
-  return Number(addNumObject.num1) + Number(addNumObject.num2);
 }//end addNum
 
 function subtractNum(){
     operation = "-";
     console.log('in subtractNum');
-  let subtractNumObject ={
-      num1: $('#firstNumber').val(),
-      num2: $('#secondNumber').val()
-  }//end addNumObject
-  return Number(subtractNumObject.num1) - Number(subtractNumObject.num2);
 }//end subtractNum
 
 function multiplyNum(){
     operation = "*";
     console.log('in multiplyNum');
-  let multiplyNumObject ={
-      num1: $('#firstNumber').val(),
-      num2: $('#secondNumber').val()
-  }//end multiplyNumObject
-  return Number(multiplyNumObject.num1) * Number(multiplyNumObject.num2);
 }//end multiplyNum
 
 function divideNum(){
     operation = "/";
     console.log('in divideNum');
-  let divideNumObject ={
-      num1: $('#firstNumber').val(),
-      num2: $('#secondNumber').val()
-  }//end divideNumObject
-  return Number(divideNumObject.num1) / Number(divideNumObject.num2);
 }//end divideNum
 
 function equalsNum(){
 let sum = 0;
+//object for input values
+let numObject ={
+    num1: $('#firstNumber').val(),
+    num2: $('#secondNumber').val()
+}//end numObject
+//conditional for math operations
 if (operation == "+") {
-    console.log(addNum()+sum);
+    console.log(sum = Number(numObject.num1) + Number(numObject.num2));
 } 
 else if (operation == "-") {
-    console.log(subtractNum()+sum);
+    console.log(sum = Number(numObject.num1) - Number(numObject.num2));
 } 
 else if (operation == "*") {
-    console.log(multiplyNum()+sum);
+    console.log(sum = Number(numObject.num1) * Number(numObject.num2));
 }  
 else if (operation == "/") {
-    console.log(divideNum()+sum);
-}   
+    console.log(sum = Number(numObject.num1) / Number(numObject.num2));
+} 
+//clear inputs
+$('#firstNumber').val('')  
+$('#secondNumber').val('')  
 }//end equalsNum
