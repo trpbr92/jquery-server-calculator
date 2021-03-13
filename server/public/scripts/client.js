47c1e84b50d10b1435fdc9dbce31e087ecf379cd
Trace Brandt <trpbr92@gmail.com>
2021-03-13 10:56:58 -0600

    operator working correctly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,35 +1,43 @@
 $(document).ready(onReady);
-
+operation = "";
 function onReady(){
     //click handlers
     $('#addButton').on('click', addNum);
     $('#subtractButton').on('click', subtractNum);
-    $('#multiplyButton').on('click', multiplyNum);
-    $('#divideButton').on('click', divideNum);
     $('#equalsButton').on('click', equalsNum);
-    $('#clearButton').on('click', clearNum);
 }//end onReady
 
+
+
 function addNum(){
+    operation = "+";
     console.log('in addNum');
-}
+  let addNumObject ={
+      num1: $('#firstNumber').val(),
+      num2: $('#secondNumber').val()
+  }//end addNumObject
+  return Number(addNumObject.num1) + Number(addNumObject.num2);
+}//end addNum
 
 function subtractNum(){
+    operation = "-";
     console.log('in subtractNum');
-}
-
-function multiplyNum(){
-    console.log('in multiplyNum');
-}
-
-function divideNum(){
-    console.log('in divideNum');
-}
+  let subtractNumObject ={
+      num1: $('#firstNumber').val(),
+      num2: $('#secondNumber').val()
+  }//end addNumObject
+  return Number(subtractNumObject.num1) - Number(subtractNumObject.num2);
+}//end subtractNum
 
 function equalsNum(){
-    console.log('in equalsNum');
-}
+let sum = 0;
+if (operation == "+") {
+    console.log(addNum()+sum);
+} 
+else if (operation == "-") {
+    console.log(subtractNum()+sum);
+}  
 
-function clearNum(){
-    console.log('in clearNum');
-}
+
+
+}//end equalsNum
